184956e29c8ba3a65ae3b4dc14f791c554c01b4f
jeswanth <jachu.k1212@gmail.com>
2026-01-11 07:46:27 +0530

    Viran_Project

---
+++ housecleaning-web/src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Services from "../Components/Services";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
