49e05d37f6bdaf5d73c4a591eb4b64fdb3dc688b
jeswanth <jachu.k1212@gmail.com>
2026-01-17 08:35:24 +0530

    Viran_Project

--- House_Cleaning/js/main.js
+++ House_Cleaning/js/main.js
@@ -61,3 +61,13 @@ function revealOnScroll() {
 
 window.addEventListener("scroll", revealOnScroll);
 revealOnScroll();
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
