34421ef04b6fc2b03ddb0d9e63d4afe7bafbb69b
jeswanth <jachu.k1212@gmail.com>
2026-01-26 13:38:16 +0530

    Viran_Project

---
+++ Chittoorcafe/src/data/menu.js
@@ -0,0 +1,31 @@
+const menu = [
+    {
+        category: "Breakfast",
+        items: [
+            { name: "Idli", price: 40, veg: true, image: "/images/menu/idli.png" },
+            { name: "Dosa", price: 50, veg: true, image: "/images/menu/dosa.png" },
+            { name: "Vada", price: 30, veg: true, image: "/images/menu/vada.png" },
+            { name: "Pongal", price: 45, veg: true, image: "/images/menu/ven-pongal-1.webp" },
+            { name: "Puri", price: 50, veg: true, image: "/images/menu/puri.jpeg" }
+        ]
+    },
+    {
+        category: "Meals",
+        items: [
+            { name: "Andhra Veg Meals", price: 120, veg: true, image: "/images/menu/meals.png" },
+            { name: "Andhra Non-Veg Meals", price: 180, veg: false, image: "/images/menu/AndhraNonvegmeals.jpeg" },
+            { name: "Ragi Sangati + Natu Kodi Pulusu", price: 220, veg: false, image: "/images/menu/Ragi-Sangati-with-Natu-Kodi-Pulusu.jpg" },
+            { name: "Chicken Biryani", price: 200, veg: false, image: "/images/menu/biryani.png" }
+        ]
+    },
+    {
+        category: "Beverages",
+        items: [
+            { name: "Filter Coffee", price: 25, veg: true, image: "/images/menu/coffee.png" },
+            { name: "Tea", price: 15, veg: true, image: "/images/menu/tea.jpeg" },
+            { name: "Badam Milk", price: 30, veg: true, image: "/images/menu/badam.jpeg" }
+        ]
+    }
+];
+
+export default menu;
