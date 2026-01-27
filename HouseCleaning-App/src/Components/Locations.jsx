0fa71b8f68dcc605e237a0ef86d6d8c2b9108fc3
jeswanth <jachu.k1212@gmail.com>
2026-01-27 14:28:53 +0530

    Viran_Project

--- HouseCleaning-App/src/Components/Locations.jsx
+++
@@ -1,8 +0,0 @@
-const Locations = () => (
-  <section className="bg-gray-100 py-16 text-center">
-    <h2 className="text-3xl font-bold mb-6">Our Locations</h2>
-    <p>Bangalore | Hyderabad</p>
-  </section>
-);
-
-export default Locations;
