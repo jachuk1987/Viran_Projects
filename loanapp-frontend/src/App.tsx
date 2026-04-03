596fcca6e4ffa530f9b66af8bfb3ccbf2d0dab7a
jeswanth <jachu.k1212@gmail.com>
2026-04-03 08:56:02 +0530

    LoanApplication

--- loanapp-frontend/src/App.tsx
+++ loanapp-frontend/src/App.tsx
@@ -2,18 +2,26 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import ApplyLoan from "./pages/ApplyLoan";
 import AdminPanel from "./pages/AdminPanel";
+import Login from "./pages/Login";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-        {/* Default Route */}
-        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/" element={<Login />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute><Dashboard /></ProtectedRoute>
+        } />
+
         <Route path="/apply" element={<ApplyLoan />} />
+
         <Route path="/admin" element={<AdminPanel />} />
+
+        <Route path="*" element={<NotFound />} />
 
       </Routes>
     </BrowserRouter>
