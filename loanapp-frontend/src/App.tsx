edf87076edbff4492507b8dc833e7ed7f79dfbd3
jeswanth <jachu.k1212@gmail.com>
2026-04-03 08:23:52 +0530

    LoanApplication

--- loanapp-frontend/src/App.tsx
+++ loanapp-frontend/src/App.tsx
@@ -4,34 +4,14 @@ import ApplyLoan from "./pages/ApplyLoan";
 import AdminPanel from "./pages/AdminPanel";
 import ProtectedRoute from "./routes/ProtectedRoute";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Login Page</h1>} />
-
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/apply"
-          element={
-            <ProtectedRoute>
-              <ApplyLoan />
-            </ProtectedRoute>
-          }
-        />
-
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/apply" element={<ApplyLoan />} />
         <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
