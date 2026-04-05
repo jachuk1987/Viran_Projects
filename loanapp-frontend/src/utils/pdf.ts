16a4a36fd21c743b990e3f6e510351db21044b08
jeswanth <jachu.k1212@gmail.com>
2026-04-05 09:09:23 +0530

    LoanApp

---
+++ loanapp-frontend/src/utils/pdf.ts
@@ -0,0 +1,25 @@
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import { getLoans } from "./storage";
+
+export const downloadPDF = () => {
+  const doc = new jsPDF();
+
+  const loans = getLoans();
+
+  const tableData = loans.map((loan: any) => [
+    loan.amount,
+    loan.type,
+    loan.status,
+  ]);
+
+  doc.text("Loan Report", 14, 15);
+
+  autoTable(doc, {
+    head: [["Amount", "Type", "Status"]],
+    body: tableData,
+    startY: 20,
+  });
+
+  doc.save("loan-report.pdf");
+};
