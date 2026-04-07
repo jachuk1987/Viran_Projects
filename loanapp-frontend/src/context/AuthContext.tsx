0ca4ce8b9e9920417bbf6e3d1456b008f0acf31d
jeswanth <jachu.k1212@gmail.com>
2026-04-07 15:44:52 +0530

    LoanApp_AuthContext

--- loanapp-frontend/src/context/AuthContext.tsx
+++ loanapp-frontend/src/context/AuthContext.tsx
@@ -1,29 +1,3 @@
-// import { createContext, useState } from "react";
-
-// export const AuthContext = createContext<any>(null);
-
-// export const AuthProvider = ({ children }: any) => {
-//   const [user, setUser] = useState(
-//     JSON.parse(localStorage.getItem("user") || "null")
-//   );
-
-//   const login = (userData: any) => {
-//     localStorage.setItem("user", JSON.stringify(userData));
-//     setUser(userData);
-//   };
-
-//   const logout = () => {
-//     localStorage.removeItem("user");
-//     setUser(null);
-//   };
-
-//   return (
-//     <AuthContext.Provider value={{ user, login, logout }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
 import { createContext, useState, ReactNode } from "react";
 
 // ✅ Define types
