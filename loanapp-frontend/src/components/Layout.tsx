c40bff2761864563f89b4722f36b3850c7e3c79c
jeswanth <jachu.k1212@gmail.com>
2026-04-08 17:49:03 +0530

    LoanApp_Layout

--- loanapp-frontend/src/components/Layout.tsx
+++ loanapp-frontend/src/components/Layout.tsx
@@ -2,15 +2,30 @@ import { Box } from "@mui/material";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
-export default function Layout({ children }: any) {
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
   return (
     <Box display="flex">
+      
+      {/* Sidebar */}
       <Sidebar />
 
+      {/* Main Content */}
       <Box flexGrow={1}>
         <Navbar />
 
-        <Box p={3}>{children}</Box>
+        <Box
+          p={3}
+          sx={{
+            backgroundColor: "#f5f6fa",
+            minHeight: "100vh",
+          }}
+        >
+          {children}
+        </Box>
       </Box>
     </Box>
   );
