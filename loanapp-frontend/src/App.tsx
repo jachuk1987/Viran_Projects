65e7f6380a83a78c27d3d0092e6411b012a0e511
jeswanth <jachu.k1212@gmail.com>
2026-04-10 08:24:42 +0530

    LoanApp

--- loanapp-frontend/src/App.tsx
+++ loanapp-frontend/src/App.tsx
@@ -1,21 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { getTheme } from "./theme";
+
 import Login from "./pages/Login";
-import AdminPanel from "./pages/AdminPanel";
 import Dashboard from "./pages/Dashboard";
 import ApplyLoan from "./pages/ApplyLoan";
+import AdminPanel from "./pages/AdminPanel";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
+  const [mode, setMode] = useState<"light" | "dark">("light");
 
-        {/* ✅ NO Layout here */}
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/apply" element={<ApplyLoan />} />
-        <Route path="/admin" element={<AdminPanel />} />
-      </Routes>
-    </BrowserRouter>
+  const toggleTheme = () => {
+    setMode((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeProvider theme={getTheme(mode)}>
+      <CssBaseline />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+
+          <Route path="/dashboard" element={<Dashboard toggleTheme={toggleTheme} />} />
+          <Route path="/apply" element={<ApplyLoan toggleTheme={toggleTheme} />} />
+          <Route path="/admin" element={<AdminPanel toggleTheme={toggleTheme} />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
