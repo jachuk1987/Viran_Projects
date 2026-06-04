3d659be7d93b694d6d6b2c6b6255515d5d4e3470
jeswanth <jachu.k1212@gmail.com>
2026-06-04 07:14:24 +0530

    Corrections

--- Uwon-Enterprises/src/components/ContactPanel.jsx
+++ Uwon-Enterprises/src/components/ContactPanel.jsx
@@ -38,7 +38,7 @@ export default function ContactPanel({ showToast }) {
                             <div>
                                 <h4>Email Address</h4>
                                 <p>jachu.k1212@gmail.com</p>
-                                <p>gnanashekar</p>
+                                <p>gnanashekar045</p>
                             </div>
                         </div>
                         <div class="method-card">
