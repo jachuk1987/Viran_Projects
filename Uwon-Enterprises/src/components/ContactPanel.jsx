b38f2db8285e00155f947a1c006620b302dc36c7
jeswanth <jachu.k1212@gmail.com>
2026-06-11 08:57:24 +0530

    ContactPanel

--- Uwon-Enterprises/src/components/ContactPanel.jsx
+++ Uwon-Enterprises/src/components/ContactPanel.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+ 
 export default function ContactPanel({ showToast }) {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
