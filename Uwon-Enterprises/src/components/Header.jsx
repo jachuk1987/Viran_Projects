d223de214bd944a05606abab567f87b2f726d5a2
jeswanth <jachu.k1212@gmail.com>
2026-06-23 08:06:30 +0530

    Header

--- Uwon-Enterprises/src/components/Header.jsx
+++ Uwon-Enterprises/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+ 
 export default function Header({ currentTab, switchTab, theme, toggleTheme }) {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
