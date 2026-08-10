a7616bfb64489f4c2328f019c9b6823d31d8b0bb
jeswanth <jachu.k1212@gmail.com>
2026-08-10 16:59:34 +0530

    StyleforApplication

--- SalesManagement-Dashboard/src/components/DashboardWrapper/index.jsx
+++ SalesManagement-Dashboard/src/components/DashboardWrapper/index.jsx
@@ -24,7 +24,7 @@ const DashboardWrapper = ({
 
                     <div className="buttons__container">
                         <div className="search__container">
-                            <IoSearch/>
+                            <IoSearch className=''/>
                             <input type="text" placeholder='Search item...' />
                         </div>
                         <div className="icon__container hover__fill">
